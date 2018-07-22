3b3e222e41384f0f2e7c1e842ea53cdd1c2fdc64
JonasZ <s0558239@htw-berlin.de>
2018-07-22 02:17:01 +0200

    finally splitting/merging is working for multiplayer; however, some problems remain (splitted elements can't eat other elements, minimap is not following player, camera does not adapt size)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,19 +12,15 @@ public class PlayerController : NetworkBehaviour
     float mergeTime = -1.0f;
     FollowPlayer followPlayer; 
     public GameObject enemySpawner; 
-    public GameObject littleBlobSpawner; 
- 
+    public GameObject littleBlobSpawner;  
+    
     EnemySpawner enemySpawnerScript; 
     LittleBlobSpawner littleBlobSpawnerScript; 
     
     // attempt to make splitting work for networking
     public GameObject playerSplitPrefab;
     List<GameObject> playerSplits = new List<GameObject>();
-
-    // source: https://answers.unity.com/questions/1295543/how-do-i-network-sync-scaling-of-character.html
-    [SyncVar(hook = "OnSetScale")]
-    private Vector3 scale;
-
+    
     Rigidbody rb;
     System.Random random = new System.Random();
 
@@ -32,7 +28,6 @@ public class PlayerController : NetworkBehaviour
     void Start () {
         rb = GetComponent<Rigidbody>();
     }
-    
     
     void Update()
     {
@@ -50,16 +45,16 @@ public class PlayerController : NetworkBehaviour
 
         enemySpawnerScript = enemySpawner.GetComponent<EnemySpawner>(); 
         littleBlobSpawnerScript = littleBlobSpawner.GetComponent<LittleBlobSpawner>(); 
+  
 
         float translation = Input.GetAxis("Vertical");
         float rot = Input.GetAxis("Horizontal");
 
-        // movement: working for host, not working for client
-        if (translation > 0) //move forwards
+        if (translation > 0) //move forward
         {
             CmdMoveForward();
         }
-        else if (translation < 0) //move backwards
+        else if (translation < 0) //move backward
         {
             CmdMoveBackward();
         }
@@ -84,31 +79,20 @@ public class PlayerController : NetworkBehaviour
 
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            CmdSplit();
+            CmdSplit(); 
         }
 
-        CmdCheckIfMerge();
+        CmdCheckIfMerge(); 
+        
     }
 
-    // source: https://answers.unity.com/questions/1295543/how-do-i-network-sync-scaling-of-character.html
-    [Command]
-    public void CmdSetScale(Vector3 vec) {
-        this.scale = vec;
-    }
-
-    // source: https://answers.unity.com/questions/1295543/how-do-i-network-sync-scaling-of-character.html
-    private void OnSetScale(Vector3 vec) {
-     this.scale = vec;
-     this.transform.localScale = vec;    
-    }
-
-    private void translateClones(float x, float y, float z) {
+    private void translateSplits(float x, float y, float z) {
         for (int i = 0; i < playerSplits.Count; i++) {
             playerSplits[i].transform.Translate(x, y, z);
         }
     }
 
-    private void rotateClones(float speed) {
+    private void rotateSplits(float speed) {
         // TODO muss aber eigener Player sein und nicht alle mit Tag Player
         var player = GameObject.FindWithTag("Player").transform;
         
@@ -118,76 +102,118 @@ public class PlayerController : NetworkBehaviour
         }
     }
 
+    // This command code is called on the client but run on the server using data from the client
     [Command]
     void CmdMoveForward() {
+        RpcMoveForward();
+    }
+
+    // runs function on all clients using data from the server
+    [ClientRpc]
+    void RpcMoveForward() {
         transform.Translate(0, 0, speed * Time.deltaTime);
-        translateClones(0, 0, speed * Time.deltaTime);
+        translateSplits(0, 0, speed * Time.deltaTime);
     }
     
     [Command]
     void CmdMoveBackward() {
+        RpcMoveBackward();
+    }
+
+    [ClientRpc]
+    void RpcMoveBackward() {
         transform.Translate(0, 0, -speed * Time.deltaTime);
-        translateClones(0, 0, -speed * Time.deltaTime);
+        translateSplits(0, 0, -speed * Time.deltaTime);
     }
 
     [Command]
     void CmdTurnRight() {
+        RpcTurnRight();
+    }
+
+    [ClientRpc]
+    void RpcTurnRight() {
         transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
-        rotateClones(rotationSpeed);        
+        rotateSplits(rotationSpeed);        
     }
 
     [Command]
     void CmdTurnLeft() {
+        RpcTurnLeft();        
+    }
+
+    [ClientRpc]
+    void RpcTurnLeft() {
         transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
-        rotateClones(-rotationSpeed);
+        rotateSplits(-rotationSpeed);
     }
 
     [Command]
     void CmdMoveUp() {
+        RpcMoveUp();
+    }
+
+    [ClientRpc]
+    void RpcMoveUp() {
         transform.Translate(0, speed / 2 * Time.deltaTime, 0);
-        translateClones(0, speed / 2 * Time.deltaTime, 0);
+        translateSplits(0, speed / 2 * Time.deltaTime, 0);
     }
 
     [Command]
     void CmdMoveDown() {
+        RpcMoveDown();
+    }
+
+    [ClientRpc]
+    void RpcMoveDown() {
         transform.Translate(0, -speed / 2  * Time.deltaTime, 0);
-        translateClones(0, -speed / 2 * Time.deltaTime, 0);
+        translateSplits(0, -speed / 2 * Time.deltaTime, 0);
     }
 
     // TODO make split for clients work for networking
+    // This command code is called on the client but run on the server using data from the client
     [Command]
     void CmdSplit()
     {
-        // Scale player
-        // old / single player: transform.localScale = transform.localScale / 2;
-        // source: https://answers.unity.com/questions/1295543/how-do-i-network-sync-scaling-of-character.html
-        CmdSetScale(transform.localScale / 2);
+        RpcSplit();
+    }
 
+    [ClientRpc]
+    // runs function on all clients using data from the server
+    // source: https://answers.unity.com/questions/1240384/how-to-sync-scale-in-unet.html
+    void RpcSplit()
+    {
+        // scale player
+        transform.localScale = transform.localScale / 2;
+        
         // Scale current splits
         for (int i = 0; i < playerSplits.Count; i++) {
-            // working for host, not working for client
             playerSplits[i].transform.localScale = playerSplits[i].transform.localScale / 2;
   
             // position current clones
             playerSplits[i].transform.Translate(-transform.localScale.x * (i + 1), 0, 0);
         }
-        
-        // save temporary amount of current splits from list
+
+        // save amount of current splits from list
         var currentPlayerSplits = playerSplits.Count;
 
         // save current position of player
         Vector3 spawnPosition = transform.position;
-
+        
+        // total width
         var xOffsetNewSplits = (currentPlayerSplits + 1) * transform.localScale.x;
         
-        // Instantiate new splits (one per current split + player)
+        // Instantiate new clones (one per current clone + player)
         for (int i = 0; i < currentPlayerSplits + 1; i++) {
-            // newSplit has initial size of prefab
+            // newSplit has initial scale of prefab
             GameObject newSplit = Instantiate(playerSplitPrefab, spawnPosition, transform.rotation);
-            // therefore: assign size of split to current size of player
+            // therefore: assign scale of split to current size of player
             newSplit.transform.localScale = transform.localScale;
 
-            newSplit.GetComponent<MeshRenderer>().material.color = Color.blue;
+            if (isLocalPlayer) {
+                newSplit.GetComponent<MeshRenderer>().material.color = Color.blue;
+            }
+            // newSplit.GetComponent<MeshRenderer>().material.color = Color.blue;
 
             // add new split to list
             playerSplits.Add(newSplit);
@@ -196,33 +222,42 @@ public class PlayerController : NetworkBehaviour
             newSplit.transform.Translate(xOffsetNewSplits + transform.localScale.x * i, 0, 0);
             
             // Spawn the newSplit on the Clients
-            NetworkServer.Spawn(newSplit);
+            // NetworkServer.Spawn(newSplit);
         }
 
         calculateSpeed();
         mergeTime = 5.0f;
     }
 
-    // This command code is called on the client but run on the server
+    // This command code is called on the client but run on the server using data from the client
     [Command]
-    void CmdCheckIfMerge()
+    void CmdCheckIfMerge() {
+        RpcCheckIfMerge();
+    }
+
+    [ClientRpc]
+    // runs function on all clients using data from the server
+    // source: https://answers.unity.com/questions/1240384/how-to-sync-scale-in-unet.html
+    void RpcCheckIfMerge() 
     {
         if(mergeTime<=0.0f && mergeTime>-0.5f){
             merge(); 
         } else if(mergeTime!=-1.0f){
-            mergeTime-=Time.deltaTime;
+            mergeTime-=Time.deltaTime; 
         }
     }
-
+    
     void merge(){
-        for(int i = 0; i < playerSplits.Count; i++){
+       for(int i = 0; i < playerSplits.Count; i++){
             transform.localScale = transform.localScale + playerSplits[i].transform.localScale; 
             Destroy(playerSplits[i]);
         }
+
         // remove all splits form the list
         playerSplits.Clear();
 
-        calculateSpeed();
+        calculateSpeed(); 
+
         mergeTime=-1.0f; 
     }
 
@@ -233,7 +268,7 @@ public class PlayerController : NetworkBehaviour
             if(transform.localScale.x>other.transform.localScale.x){
                 other.gameObject.SetActive(false);
                 //status.scoreKilledOtherPlayer(5);
-                scaleUp(other.gameObject.transform.localScale.y*0.7f); 
+                CmdScaleUp(other.gameObject.transform.localScale.y*0.7f); 
                 enemySpawnerScript.createEnemy(1);  
             }
         }
@@ -241,7 +276,7 @@ public class PlayerController : NetworkBehaviour
         {
             other.gameObject.SetActive(false);
             //status.scoreAbsorbedLittleBlob();
-            scaleUp(other.gameObject.transform.localScale.y);  
+            CmdScaleUp(other.gameObject.transform.localScale.y);  
             littleBlobSpawnerScript.createLittleBlob(1);
         } 
         else if (other.gameObject.CompareTag("Player"))
@@ -251,11 +286,9 @@ public class PlayerController : NetworkBehaviour
                 //call method to let player know he died --> show menu with options respawn and exit
                 //other.getComponent<PlayerController>().died();
                 other.gameObject.SetActive(false);
-                scaleUp(other.gameObject.transform.localScale.y);
-            }
-            
+                CmdScaleUp(other.gameObject.transform.localScale.y);
+            }   
         }
-        
     }
 
     void calculateSpeed(){
@@ -272,19 +305,26 @@ public class PlayerController : NetworkBehaviour
         // Camera
         followPlayer = Camera.main.GetComponent<FollowPlayer>();
         followPlayer.player = gameObject.transform;
-        Minimap minimapCam = GameObject.FindGameObjectWithTag("minimapCam").GetComponent<Minimap>(); 
-        minimapCam.player = gameObject; 
+        MinimapScript minimapCam = GameObject.FindGameObjectWithTag("minimapCam").GetComponent<MinimapScript>(); 
+        
+        // TODO does not work any more
+        minimapCam.player = gameObject;
     }
 
-    private void scaleUp(float size)
-    {
-        // TODO: wenn split was isst, wird aber player groesser
+    [Command]
+    void CmdScaleUp(float size) {
+        RpcScaleUp(size);
+    }
 
+    [ClientRpc]
+    void RpcScaleUp(float size)
+    {
         // scale up player
         Vector3 newScale = new Vector3(transform.localScale.x + size, transform.localScale.y + size, transform.localScale.z + size);
         transform.localScale = Vector3.Slerp(transform.localScale, newScale, slerpTime); 
         
-        adaptCameraOffset(1 + (size/4)); 
+        // TODO does not work any more
+        adaptCameraOffset(1 + size/4); 
         calculateSpeed();
     }
  
@@ -292,7 +332,7 @@ public class PlayerController : NetworkBehaviour
     {
         Vector3 adaptedDistance = new Vector3(followPlayer.distance.x * adaption, followPlayer.distance.y * adaption, followPlayer.distance.z * adaption);
         followPlayer.distance = Vector3.Slerp(followPlayer.distance, adaptedDistance, slerpTime);
-    }
+     }
 
     private void won(){
         //TODO: Tell player that he won
